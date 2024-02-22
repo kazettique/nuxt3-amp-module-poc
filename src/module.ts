37dcd1a7404af01196953c1b53140be5a0128a7d
Woody Chang <kazettique@gmail.com>
2024-02-22 21:23:10 +0800

    test

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,41 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import {
+  defineNuxtModule,
+  addPlugin,
+  createResolver,
+  addServerPlugin,
+} from "@nuxt/kit";
+
+const AMPBoilerplate =
+  "<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>" +
+  '<script async src="https://cdn.ampproject.org/v0.js"></script>';
+
+const scriptPattern = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+const ampBodyPattern = /<amp-body[^>]*>([.\S\s]*)<\/amp-body>/;
+
+const styleLoaders = [
+  {
+    test: /\.scss$/i,
+    loader: "!sass-loader",
+  },
+];
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: "my-module",
+    configKey: "myModule",
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
+  setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
+
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
-})
+    // addPlugin(resolver.resolve("./runtime/plugin"));
+    addServerPlugin(resolver.resolve("./runtime/plugin"));
+  },
+});
+
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,122 @@
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin } from "#app";
+import { useRuntimeConfig } from "#imports";
+import { createResolver, defineNuxtModule, addServerPlugin } from "@nuxt/kit";
+import { defineNitroPlugin } from "nitropack/dist/runtime/plugin";
 
-export default defineNuxtPlugin((nuxtApp) => {
-  console.log('Plugin injected by my-module!')
-})
+// export default defineNuxtPlugin((nuxtApp) => {
+//   console.log("Plugin injected by my-module!");
+// });
+
+// export default defineNitroPlugin((nuxtApp) => {
+//   console.log("Plugin injected by server nitro plugin!");
+// });
+
+const JS_HINT_RE = /<[^>]+(\.m?js"|as="script")[^>]*>/g;
+
+export default defineNitroPlugin((nitroApp) => {
+  nitroApp.hooks.hook("render:html", (htmlContext, { event }) => {
+    // console.log('htmlContext', htmlContext)
+
+    const hasAMPPrefix: boolean =
+      event.path === "/amp" || event.path.indexOf("/amp/") === 0;
+
+    htmlContext.htmlAttrs = ["amp"];
+    htmlContext.head = [
+      '<meta charset="utf-8">',
+      "<title>Document123</title>",
+      '<script async src="https://cdn.ampproject.org/v0.js"></script>',
+      '<link rel="canonical" href="https://amp.dev/documentation/examples/introduction/hello_world/index.html">',
+      '<meta name="viewport" content="width=device-width">',
+      /* html */ `
+      <style amp-custom>
+        h1 {
+          color: red;
+        }
+      </style>
+      `,
+      /* html */ `
+        <style amp-boilerplate>
+          body {
+            -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+          }
+
+          @-webkit-keyframes -amp-start {
+            from {
+              visibility: hidden;
+            }
+            to {
+              visibility: visible;
+            }
+          }
+          @-moz-keyframes -amp-start {
+            from {
+              visibility: hidden;
+            }
+            to {
+              visibility: visible;
+            }
+          }
+          @-ms-keyframes -amp-start {
+            from {
+              visibility: hidden;
+            }
+            to {
+              visibility: visible;
+            }
+          }
+          @-o-keyframes -amp-start {
+            from {
+              visibility: hidden;
+            }
+            to {
+              visibility: visible;
+            }
+          }
+          @keyframes -amp-start {
+            from {
+              visibility: hidden;
+            }
+            to {
+              visibility: visible;
+            }
+          }
+        </style>
+        <noscript>
+          <style amp-boilerplate>
+            body {
+              -webkit-animation: none;
+              -moz-animation: none;
+              -ms-animation: none;
+              animation: none;
+            }
+          </style>
+        </noscript>
+      `,
+    ];
+    htmlContext.body = ['<h1>hacked by plugin!!</h1>'];
+
+    // filter nuxt script
+
+    const $config = useRuntimeConfig();
+    const ASSET_RE = new RegExp(
+      `<script[^>]*src="${$config.app.buildAssetsDir}[^>]+><\\/script>`
+    );
+
+    htmlContext.bodyAppend = htmlContext.bodyAppend.filter(
+      (b) => !b.includes("window.__NUXT__") && !ASSET_RE.test(b)
+    );
+    const i = htmlContext.head.findIndex((i) =>
+      i.includes('<link rel="modulepreload" as="script"')
+    );
+    if (i !== -1) {
+      htmlContext.head[i] = htmlContext.head[i].replace(JS_HINT_RE, "");
+    }
+  });
+
+  // nitroApp.hooks.hook('render:response', (response, { event }) => {
+  //   console.log('render:response', response);
+  // });
+});
